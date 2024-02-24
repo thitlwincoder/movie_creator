bdbcc9ce330d69479dbad9ea5b8bcdae4b9dd1f7
thitlwincoder <thitlwincoder@gmail.com>
2024-02-24 08:29:31 +0630

    feat: initial commit

---
+++ lib/moviepy_flutter.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library moviepy_flutter;
+
+export 'src/moviepy_flutter.dart';
---
+++ lib/src/moviepy_flutter.dart
@@ -0,0 +1,7 @@
+/// {@template moviepy_flutter}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class MoviepyFlutter {
+  /// {@macro moviepy_flutter}
+  const MoviepyFlutter();
+}
